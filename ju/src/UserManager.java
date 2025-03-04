a95cd997af5a750a3344902b91ec7d8afaf488c8
andirahim <burcifiqi@gamil.com>
2025-03-04 21:46:03 +0300

    commit #2
    in the nv, on line 6 Method to add a user is created, on line 9 Method to display all users is added, on line 10 Iterate over users list and on line 11 in printed out

--- ju/src/UserManager.java
+++ ju/src/UserManager.java
@@ -3,3 +3,12 @@ import java.util.ArrayList;
 import java.util.List;
 public class UserManager {
     private List<User> users = new ArrayList<>();
+    public void addUser(User user) {
+        users.add(user);
+    }
+    public void displayUsers() {
+        for (User user : users) {
+            System.out.println(user);
+        }
+    }
+}
