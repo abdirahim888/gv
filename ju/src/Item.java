91e030ea507de307f5b87ebe2edea6954a3bf64b
andirahim <burcifiqi@gamil.com>
2025-03-04 21:51:15 +0300

    commit #1
    in the nv, on line the package for item management is defined , on line 2 the Item class is defined, on line 3 the item's name is stored, on line 4 the item's price is stored , on line 5 Constructor to initialize item details is added

---
+++ ju/src/Item.java
@@ -0,0 +1,8 @@
+package item;
+public class Item {
+    private String name;
+    private double price;
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
